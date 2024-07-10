91a20d2f0acdfe8304495d5cd3e4211c62239ab7
Mondip <91971707+mondipda@users.noreply.github.com>
2024-07-10 10:19:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,19 +14,5 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer","Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
     
 });
